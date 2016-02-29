380dc958068b35aabade35829765419cd661bed5
Kyle Scully <scullykns@gmail.com>
2016-02-28 21:51:24 -0800

    fixed totalling to use string match

--- web/static/vendor/totalling.js
+++ web/static/vendor/totalling.js
@@ -1,33 +1,34 @@
 
 function total_per_month(cost_col,total_col) {
-  var row_cost = 0
+  var row_cost = 0;
     $('tr').each(function () {
         var cost = $(this).find(cost_col).text();
         if (!isNaN(cost) && cost.length !== 0) {
             cost = parseFloat(cost);
             // console.log(cost)
-        };
+        }
         var freq = $(this).find('.frequency').text();
-        switch(freq) {
-          case "Monthly":
+        console.log("freq: " + freq);
+        switch(true) {
+          case (freq.indexOf("Monthly") !=-1):
               freq = 1;
               break;
-          case "Yearly":
+          case (freq.indexOf("Yearly") !=-1):
               freq = 1/12;
               break;
-          case "Daily":
+          case (freq.indexOf("Daily") !=-1):
               freq = 30;
               break;
-          case "Weekly":
+          case (freq.indexOf("Weekly") !=-1):
               freq = 4;
               break;
-          case "One Time Payment":
+          case (freq.indexOf("One Time Payment") !=-1):
               freq = 1;
               break;
           default:
               freq = 1;
-        };
-        // console.log(freq)
+        }
+        console.log("freq: " + freq);
         row_cost = row_cost + (cost*freq);
     });
 return $(total_col).text(row_cost.toFixed(2));
